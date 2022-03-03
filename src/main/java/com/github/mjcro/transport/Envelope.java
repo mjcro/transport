043ba729455a9e8d8b52d7e8fe7025e5bac5681c
Gotterdemarung <gotterdemarung@gmail.com>
2022-03-03 18:54:42 +0200

    :sparkles: Headers

--- src/main/java/com/github/mjcro/transport/Envelope.java
+++ src/main/java/com/github/mjcro/transport/Envelope.java
@@ -2,17 +2,53 @@ package com.github.mjcro.transport;
 
 import com.github.mjcro.references.bytes.ByteBodyReference;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public interface Envelope extends ByteBodyReference {
     /**
      * @return Envelope headers, unmodifiable map.
      */
-    Map<String, String> getHeaders();
+    Map<String, List<String>> getHeaders();
 
     /**
-     * @return Full envelope headers, unmodifiable map.
+     * Fetches header data by header name.
+     * By convention, header lookup should be case-insensitive.
+     *
+     * @param name Header name, case-insensitive.
+     * @return Matched header data or empty list.
      */
-    Map<String, List<String>> getFullHeaders();
+    List<String> getHeader(String name);
+
+    /**
+     * @return Headers as ordinary map.
+     */
+    default Map<String, String> getFlatHeaders() {
+        Map<String, List<String>> headers = getHeaders();
+        if (headers.isEmpty()) {
+            return Collections.emptyMap();
+        }
+        HashMap<String, String> flat = new HashMap<>(headers.size());
+        for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
+            List<String> value = entry.getValue();
+            if (value != null && !value.isEmpty()) {
+                flat.put(entry.getKey(), value.get(0));
+            }
+        }
+        return flat;
+    }
+
+    /**
+     * Fetches header data by header name.
+     * By convention, header lookup should be case-insensitive.
+     *
+     * @param name Header name, case-insensitive.
+     * @return Matched header data or empty optional.
+     */
+    default Optional<String> getFlatHeader(String name) {
+        return getHeader(name).stream().findAny().filter($header -> !$header.isEmpty());
+    }
 }
--- src/main/java/com/github/mjcro/transport/EnvelopeImpl.java
+++ src/main/java/com/github/mjcro/transport/EnvelopeImpl.java
@@ -1,39 +1,34 @@
 package com.github.mjcro.transport;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 class EnvelopeImpl implements Envelope {
-    private final Map<String, String> headers;
-    private final Map<String, List<String>> full;
+    private final Map<String, List<String>> headers;
     private final byte[] body;
 
-    EnvelopeImpl(Map<String, String> headers, Map<String, List<String>> full, byte[] body) {
+    EnvelopeImpl(Map<String, List<String>> headers, byte[] body) {
         if (headers != null && !headers.isEmpty()) {
-            this.headers = Collections.unmodifiableMap(new HashMap<>(headers));
+            TreeMap<String, List<String>> headersData = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+            headersData.putAll(headers);
+            this.headers = Collections.unmodifiableMap(headersData);
         } else {
             this.headers = Collections.emptyMap();
-        }
-        if (full != null && !full.isEmpty()) {
-            this.full = Collections.unmodifiableMap(new HashMap<>(full));
-        } else {
-            this.full = Collections.emptyMap();
         }
 
         this.body = body == null ? new byte[0] : body;
     }
 
-
     @Override
-    public Map<String, String> getHeaders() {
+    public Map<String, List<String>> getHeaders() {
         return headers;
     }
 
     @Override
-    public Map<String, List<String>> getFullHeaders() {
-        return full;
+    public List<String> getHeader(String name) {
+        return headers.getOrDefault(name, Collections.emptyList());
     }
 
     @Override
