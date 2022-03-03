c2cf863831bb60dd911154b60f0acd671ceb73ea
Gotterdemarung <gotterdemarung@gmail.com>
2022-03-03 19:16:47 +0200

    :sparkles: Request and response

--- src/main/java/com/github/mjcro/transport/Response.java
+++ src/main/java/com/github/mjcro/transport/Response.java
@@ -2,7 +2,36 @@ package com.github.mjcro.transport;
 
 import com.github.mjcro.references.time.ElapsedReference;
 
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public interface Response extends Envelope, ElapsedReference {
+    static Response create(Map<String, List<String>> headers, String address, byte[] body, int code, Duration elapsed) {
+        return new ResponseImpl(headers, address, body, code, elapsed);
+    }
+
+    static Response empty(String address) {
+        return create(Collections.emptyMap(), address, new byte[0], 0, Duration.ZERO);
+    }
+
+    static Response createPlain(Map<String, String> headers, String address, byte[] body, int code, Duration elapsed) {
+        return new ResponseImpl(
+                headers == null || headers.isEmpty()
+                        ? Collections.emptyMap()
+                        : headers.entrySet().stream().collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        $entry -> Collections.singletonList($entry.getValue())
+                )),
+                address,
+                body,
+                code,
+                elapsed
+        );
+    }
+
     /**
      * @return Address, URL in most cases.
      */
