629b3f1990d12b6e7d393983d40423aa3daaf7e2
Gotterdemarung <gotterdemarung@gmail.com>
2025-01-01 00:21:54 +0200

    :sparkles: Void telemetry consumer

---
+++ src/main/java/io/github/mjcro/transport/VoidTelemetryConsumer.java
@@ -0,0 +1,19 @@
+package io.github.mjcro.transport;
+
+import io.github.mjcro.interfaces.durations.WithElapsed;
+import io.github.mjcro.interfaces.experimental.integration.Packet;
+import io.github.mjcro.interfaces.experimental.integration.Telemetry;
+import io.github.mjcro.interfaces.experimental.integration.TelemetryConsumer;
+
+import java.time.temporal.TemporalAccessor;
+
+/**
+ * Telemetry consumer implementation that does nothing.
+ */
+public class VoidTelemetryConsumer<Req extends Packet, Res extends Packet & WithElapsed, Meta, T extends TemporalAccessor>
+        implements TelemetryConsumer<Req, Res, Meta, T> {
+    @Override
+    public void accept(final Telemetry<? extends Req, ? extends Res, ? extends Meta, ? extends T> telemetry) {
+        // Do nothing
+    }
+}
