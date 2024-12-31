fa50c3d4c4c28ec7bd390e67071b08da6267c433
Gotterdemarung <gotterdemarung@gmail.com>
2025-01-01 00:22:16 +0200

    :sparkles: Options utility

---
+++ src/main/java/io/github/mjcro/transport/options/Options.java
@@ -0,0 +1,35 @@
+package io.github.mjcro.transport.options;
+
+import io.github.mjcro.interfaces.experimental.integration.Option;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Options {
+    private Options() {
+    }
+
+    /**
+     * Merges two options array.
+     *
+     * @param a Options array.
+     * @param b Options array.
+     * @return resulting array.
+     */
+    public static Option[] merge(Option[] a, Option[] b) {
+        if ((a == null || a.length == 0) && (b == null || b.length == 0)) {
+            // Both empty
+            return new Option[0];
+        }
+        if (a == null || a.length == 0) {
+            return b;
+        }
+        if (b == null || b.length == 0) {
+            return a;
+        }
+        ArrayList<Option> result = new ArrayList<>(a.length + b.length);
+        result.addAll(Arrays.asList(a));
+        result.addAll(Arrays.asList(b));
+        return result.toArray(new Option[0]);
+    }
+}
